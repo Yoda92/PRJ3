c11002714e65288a75e58a1a6fc4f91f9ab98f54
Your Name <you@example.com>
2019-11-11 12:47:22 +0100

    Temp

---
+++ fase_3/sender/inputHandler.cpp
@@ -0,0 +1,58 @@
+#include "inputHandler.hpp"
+
+// Global values
+int inputHandler::throttleUp = 0;
+int inputHandler::throttleDown = 0;
+int inputHandler::toggleswitch = 0;
+int inputHandler::adc0 = 0;
+int inputHandler::adc1 = 0;
+
+// Device address buffers
+char inputHandler::buffer_throttle_up[PATH_LENGTH_GPIO] = {0};
+char inputHandler::buffer_throttle_down[PATH_LENGTH_GPIO] = {0};
+char inputHandler::buffer_switch[PATH_LENGTH_GPIO] = {0};
+char inputHandler::buffer_adc[PATH_LENGTH_ADC] = {0};
+
+void inputHandler::init(void)
+{
+    // Set device address variables
+    snprintf(buffer_throttle_up, PATH_LENGTH_GPIO, "/sys/class/gpio/gpio%d/value", GPIO_THROTTLE_UP);
+    snprintf(buffer_throttle_down, PATH_LENGTH_GPIO, "/sys/class/gpio/gpio%d/value", GPIO_THROTTLE_DOWN);
+    snprintf(buffer_switch, PATH_LENGTH_GPIO, "/sys/class/gpio/gpio%d/value", GPIO_SWITCH);
+    snprintf(buffer_adc, PATH_LENGTH_ADC, "/dev/spi_drv0");
+}
+
+void inputHandler::updateInput(void)
+{
+    // Read GPIO values
+    readValue(buffer_throttle_up, 2, throttleUp);
+    readValue(buffer_throttle_down, 2, throttleDown);
+    readValue(buffer_switch, 2, toggleswitch);
+    // Read both ADC channels
+    writeValue(buffer_adc, "0");
+    readValue(buffer_adc, 5, adc0); 
+    writeValue(buffer_adc, "1");
+    readValue(buffer_adc, 5, adc1); 
+}
+
+void inputHandler::readValue(char *buffer, int size, int &input)
+{
+    char readdata[size];
+    // Set filedescripters and open device
+    int fd = open(buffer, O_RDONLY);
+    // read value to readdata
+    read(fd, readdata, size);
+    input = atoi(readdata);
+    // Close filedescriptor
+    close(fd);
+}
+
+void inputHandler::writeValue(char *buffer, const char * value)
+{
+    // Set filedescripters and open device
+    int fd = open(buffer, O_WRONLY);
+    // write value to device
+    write(fd, (const void *) value, strlen(value));
+    // Close filedescriptor
+    close(fd);
+}
---
+++ fase_3/sender/main.cpp
@@ -0,0 +1,28 @@
+#include "controller.hpp"
+#include "inputHandler.hpp"
+#include "outputHandler.hpp"
+
+int main(void)
+{
+    inputHandler::init();
+    outputHandler::init();
+    // Program loop
+    while (true)
+    {
+        inputHandler::updateInput();
+        printf("Input: %d, %d, %d, %d, %d\n", inputHandler::throttleUp, inputHandler::throttleDown, inputHandler::toggleswitch, inputHandler::adc0, inputHandler::adc1);
+        int x;
+        controller::createCommand(
+            inputHandler::throttleUp, 
+            inputHandler::throttleDown, 
+            inputHandler::toggleswitch,
+            inputHandler::adc0,
+            inputHandler::adc1
+            );
+        printf("Output: %d\n", controller::commandByte);
+        outputHandler::sendOutput(controller::commandByte);
+        printf("%d", x);
+        x++;
+    }
+    return 0;
+}
