4614cb1aaac3699efe7d29306d083c3e0b6b31d7
Your Name <you@example.com>
2019-11-26 10:55:09 +0100

    Fix

--- fase_3/receiver/dir/autostart_debug.sh
+++ fase_3/receiver/dir/autostart_debug.sh
@@ -2,32 +2,25 @@
 
 SSID=rpiWifi
 PW=wifi1234
-rm /var/lib/connman/*psk* -R
+
+sleep 30
+connmanctl disable wifi
+connmanctl enable wifi
 
 while true
 do
+    echo "Scanning network..."
+    connmanctl scan wifi
+    HASH=$(connmanctl services | grep $SSID | awk '{print $3}')
+    if [ "$HASH" == "" ] 
+    then 
+        HASH=$(connmanctl services | grep $SSID | awk '{print $2}')
+    fi
     # Check for connection
-    if ping 192.168.0.1 -w 1 | grep -q '100% packet loss'
+    if [ "$HASH" != "" ] 
     then
-        echo "No connection. Attempting to connect..." 
-        connmanctl disable wifi
-        connmanctl enable wifi
-        connmanctl scan wifi
-        echo "Scanning network..." 
-        # Run services --> Find WIFI with SSID --> Set Hash to third or second field
-        HASH=$(connmanctl services | grep $SSID | awk '{print $3}')
-        if [ "$HASH" == "" ] 
-        then 
-            HASH=$(connmanctl services | grep $SSID | awk '{print $2}')
-            echo $HASH
-        fi
-        if [ "$HASH" == "" ] 
-        then
-            echo "WIFI: $SSID was not found"
-        else
-            echo "Discovered WIFI: $SSID with hash: $HASH"
-            sleep 2
-            echo "Writing config file for WIFI: $SSID"
+        echo "Discovered WIFI: $SSID with hash: $HASH"
+        echo "Writing config file for WIFI: $SSID"
 # <-------- Indent error due to bash EOF error
 cat << EOF > /var/lib/connman/$SSID-psk.config
 [service_$HASH]
@@ -35,16 +28,15 @@ Type = wifi
 Name = $SSID
 Passphrase = $PW
 EOF
-sleep 2
 # <-------- Indent error due to bash EOF error
-            connmanctl connect $HASH
-            echo "Connection test gogogogo"
+            connmanctl connect $HASH 
+            echo "Connected. Starting program.."
+            sleep 2
             ~/main
-            fi 
+            echo "Program terminated."
     else
-        echo "Connected. Starting program.."
-        ~/main
-        echo "Program terminated. Sleeping for 2 seconds..."
+        echo "AP not found..."
     fi
+    echo "Sleeping for 2 seconds..."
     sleep 2
 done
